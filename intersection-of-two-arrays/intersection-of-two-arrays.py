ef8c89cafd18e3f97ec8247e5f9fd777c531f647
Amlan Alok <amlanalok@gmail.com>
2021-12-27 01:17:14 -0600

    Update intersection-of-two-arrays.py

--- intersection-of-two-arrays/intersection-of-two-arrays.py
+++ intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -2,6 +2,9 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return self.ans_2(nums1, nums2)
     
+    # TC = O(n+m) average case
+    # TC = O(n*m) worst case
+    # SC = O(1)
     def ans_2(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         print(set(nums1).intersection(set(nums2)))
