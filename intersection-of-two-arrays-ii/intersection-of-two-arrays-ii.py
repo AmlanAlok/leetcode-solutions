ea909fb11dd584b54af9673d45ee32abe1e44722
Amlan Alok <amlanalok@gmail.com>
2021-12-27 08:14:05 -0600

    Update intersection-of-two-arrays-ii.py

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,11 +1,17 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return self.ans_1(nums1, nums2)
-        
+    
+    
+    # TC = O(n+m)
+    # SC = O(min(n, m))
     def ans_1(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
+        if len(nums1) > len(nums2):
+            return self.ans_1(nums2, nums1)
+        
         dict = {}
-        a = []
+        k = 0
         
         for i, v in enumerate(nums1):
             if v in dict:
@@ -18,9 +24,12 @@ class Solution:
         
         for i, v in enumerate(nums2):
             if v in dict and dict[v] > 0:
-                a.append(v)
+                nums1[k] = v
+                k += 1
                 dict[v] -= 1
         
-        return a
+        return nums1[:k]
+                
+        
                 
         
