79e847449a23b86780d5e901a44e6046f6a4db1b
Amlan Alok <amlanalok@gmail.com>
2021-12-29 19:08:30 +0530

    added follow up changes

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -34,7 +34,7 @@ class Solution:
         
         return nums1[:k]
         
-    
+        
 # TC = O(n+m)
 # SC = O(min(n, m))
     def ans_1(self, nums1: List[int], nums2: List[int]) -> List[int]:
@@ -53,7 +53,6 @@ class Solution:
                 
         print(dict)
         
-        
         for i, v in enumerate(nums2):
             if v in dict and dict[v] > 0:
                 nums1[k] = v
@@ -61,3 +60,24 @@ class Solution:
                 dict[v] -= 1
         
         return nums1[:k]
+    
+    
+
+# Follow-up Questions
+
+# What if the given array is already sorted? How would you optimize your algorithm?
+
+# We can use either Approach 2 or Approach 3, dropping the sort of course. It will give us linear time and constant memory complexity.
+# What if nums1's size is small compared to nums2's size? Which algorithm is better?
+
+# Approach 1 is a good choice here as we use a hash map for the smaller array.
+# What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+# If nums1 fits into the memory, we can use Approach 1 to collect counts for nums1 into a hash map. Then, we can sequentially load and process nums2.
+
+# If neither of the arrays fit into the memory, we can apply some partial processing strategies:
+
+# Split the numeric range into subranges that fits into the memory. Modify Approach 1 to collect counts only within a given subrange, and call the method multiple times (for each subrange).
+
+# Use an external sort for both arrays. Modify Approach 2 to load and process arrays sequentially.
+
