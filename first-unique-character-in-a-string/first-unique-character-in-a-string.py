b34069233e6009534d2ea0b2548681bdb004ae6e
Amlan Alok <amlanalok@gmail.com>
2022-01-01 15:51:54 +0530

    Update first-unique-character-in-a-string.py

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -2,6 +2,27 @@ class Solution:
     def firstUniqChar(self, s: str) -> int:
         return self.ans_1(s)
     
+    # Horrible code from the comments section. ord function is most likely causing the issue
+    def ans_2(self, s: str) -> int:
+        
+        a = [0]*26
+        
+        for i in range(len(s)):
+            p = ord(s[i]) - ord('a')
+            print(p)
+            a[p] += 1
+        
+        for i in range(len(s)):
+            p = ord(s[i]) - ord('a')
+            print(p)
+            if a[p] == 1:
+                return i
+        
+        return -1
+        
+    
+    # TC = O(N)
+    # SC = O(1) -> This is not N as english has only 26 alphabets
     def ans_1(self, s: str) -> int:
         
         dict = {}
