1a00e6a883cbc9e77910648c95d384527cb203d4
Amlan Alok <amlanalok@gmail.com>
2022-01-02 23:53:20 +0530

    Neat return trick

--- first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,6 +1,22 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        return self.ans_1(s)
+        return self.ans_3(s)
+    
+    # code from solution with fastest time
+    def ans_3(self, s: str) -> int:
+        
+        min_index = len(s)
+        
+        for c in 'abcdefghijklmnopqrstuvwxyz':
+            print(c)
+            i = s.find(c)       # TC = O(n*m), n = length of string and length of substring to find, returns first positioin, else -1
+            x = s.rfind(c)      # TC = O(n*m), n = length of string and length of substring to find, returns last position, else -1
+            # print(i, x)
+            
+            if i != -1 and i == x:
+                min_index = min(min_index, i)
+        
+        return min_index if min_index != len(s) else -1     # This is a neat trick to memorize
     
     # Horrible code from the comments section. ord function is most likely causing the issue
     def ans_2(self, s: str) -> int:
