7832cfc6fa9b4ab201bdaf0cfecdbd68f7137388
Amlan Alok <amlanalok@gmail.com>
2022-01-04 03:41:35 +0530

    Time: 63 ms (23.37%), Space: 14.4 MB (81.08%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        return self.ans_2(s,t)
+        
+    # TC = O(n+m)
+    # SC = O(n+m)
+    def ans_2(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        
+        d = collections.Counter(t)
+        a = collections.Counter(s)
+        
+        if a != d:
+            return False
+        return True
+    
+    # TC = O(n+m)
+    # SC = O(m)
+    def ans_1(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        d = collections.Counter(t)
+        
+        for i in s:
+            if i not in d:
+                return False
+            
+            if d[i] <= 0:
+                return False
+            
+            d[i] -= 1
+            
+        return True
+            
+            
+        
