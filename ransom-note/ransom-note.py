c3dc5450936f9213bed6cab68d6f6925142c1a2d
Amlan Alok <amlanalok@gmail.com>
2022-01-03 10:26:32 +0530

    Update ransom-note.py

--- ransom-note/ransom-note.py
+++ ransom-note/ransom-note.py
@@ -2,11 +2,33 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return self.ans_1(ransomNote, magazine)
+        return self.ans_2(ransomNote, magazine)
+    
+    def ans_2(self, ransomNote: str, magazine: str) -> bool:
         
+        if len(magazine) < len(ransomNote):     # Edge case scenario which can optimize the code
+            return False
+        
+        dict = Counter(magazine)            # TC = O(m)
+        print(len(dict))
+        
+        for i in ransomNote:
+            
+            if i not in dict:
+                return False
+            
+            if dict[i] <=0:
+                return False
+            
+            dict[i] -= 1
+            
+        return True
+    
+    # TC = O(n+m)
+    # SC = O(m)
     def ans_1(self, ransomNote: str, magazine: str) -> bool:
         
-        dict = Counter(magazine)
+        dict = Counter(magazine)            # TC = O(m)
         print(dict)
         
         for i in ransomNote:
