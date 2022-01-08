04060fcfc11b29730a6cb9fbac05aa8401139fe9
Amlan Alok <amlanalok@gmail.com>
2022-01-08 12:10:00 +0530

    Time: 77 ms (20.85%), Space: 18.2 MB (7.58%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -14,27 +14,27 @@ class Solution:
         
     def ans_1(self, head: Optional[ListNode]) -> bool:
         
-#         d = set()
-#         c = head
+        d = set()
+        c = head
         
-#         while c is not None:
-#             if c in d:
-#                 return True
-#             d.add(c)
-#             print(d)
-#             c = c.next
-        
-#         print(d)
-        
-#         return False
-
-        nodes_seen = set()
-        while head is not None:
-            if head in nodes_seen:
+        while c is not None:
+            if c in d:
                 return True
-            nodes_seen.add(head)
-            head = head.next
+            d.add(c)
+            # print(d)
+            c = c.next
+        
+        # print(d)
+        
         return False
+
+        # nodes_seen = set()
+        # while head is not None:
+        #     if head in nodes_seen:
+        #         return True
+        #     nodes_seen.add(head)
+        #     head = head.next
+        # return False
             
     
         
