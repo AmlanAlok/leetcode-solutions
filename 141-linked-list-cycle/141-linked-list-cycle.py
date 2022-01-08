2c6917274693dc23b932b68f9b39edd25da60c83
Amlan Alok <amlanalok@gmail.com>
2022-01-08 12:08:47 +0530

    Time: 85 ms (16.08%), Space: 18.1 MB (12.82%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# Q How many max elements can be in the linked list?
+# Q What is the min and max value of val?
+# Q Will all the entries be unique?
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        return self.ans_1(head)
+        
+    def ans_1(self, head: Optional[ListNode]) -> bool:
+        
+#         d = set()
+#         c = head
+        
+#         while c is not None:
+#             if c in d:
+#                 return True
+#             d.add(c)
+#             print(d)
+#             c = c.next
+        
+#         print(d)
+        
+#         return False
+
+        nodes_seen = set()
+        while head is not None:
+            if head in nodes_seen:
+                return True
+            nodes_seen.add(head)
+            head = head.next
+        return False
+            
+    
+        
