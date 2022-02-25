2c60eae0ff807f104122e4632dc36346f97f0442
Amlan Alok <amlanalok@gmail.com>
2022-02-25 07:27:27 +0530

    Time: 85 ms (48.75%), Space: 14.3 MB (51.68%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,7 +1,34 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         
-        return self.ans1(nums, n)
+        return self.ans2(nums, n)
+    
+    def ans2(self, nums: List[int], n: int) -> List[int]:
+        from collections import deque
+        
+        i=1
+        j=n
+        
+        t=deque()
+
+        while i<2*n-1:
+            c = nums[i]
+            
+            if c != -1:
+                t.append(c)
+            # from x
+            if i%2 == 0:
+                nums[i] = t.popleft()
+            # from y
+            else:
+                nums[i] = nums[j]
+                nums[j]=-1
+                j+=1
+            i+=1
+        
+        return nums
+        
+        
         
     def ans1(self, nums: List[int], n: int) -> List[int]:
         
