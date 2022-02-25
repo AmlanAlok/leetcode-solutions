d39e9597359c73d22e36ad7ec679b76d54ccbaa7
Amlan Alok <amlanalok@gmail.com>
2022-02-26 02:30:32 +0530

    Time: 153 ms (28.67%), Space: 15 MB (5.53%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,11 +1,40 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        return self.ans1(numbers, target)
+        return self.ans3(numbers, target)
     
+    
+    def ans3(self, numbers: List[int], target: int) -> List[int]:
+        
+        nums=numbers
+        i=0
+        j=len(nums)-1
+        
+        while i<j:
+            s=nums[i]+nums[j]
+            
+            if s>target:
+                j-=1
+            elif s<target:
+                i+=1
+            else:
+                return [i+1, j+1]
+            
+        return [-1,-1]
+    
+    '''
+    This solution does not work. It is incorrect.
+    '''
     def ans2(self, numbers: List[int], target: int) -> List[int]:
         
         nums = numbers
+        
+        for i in range(len(nums)):
+            if nums[i] == target:
+                if i+1<len(nums) and nums[i]+nums[i+1] == target:
+                    print('does not work')
+        
+        
         i=0
         j=0
         while nums[i]<target:
