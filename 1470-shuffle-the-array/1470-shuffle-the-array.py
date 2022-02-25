b0f37f0783749a0bba263322a931b2fa4d0fc1ef
Amlan Alok <amlanalok@gmail.com>
2022-02-25 06:47:40 +0530

    Time: 75 ms (60.80%), Space: 14.1 MB (96.87%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        
+        x = nums[:n]
+        y = nums[n:]
+        
+        j, k =0, 0
+        for i in range(2*n):
+            if i%2 == 0:
+                nums[i] = x[k]
+                k+=1
+            else:
+                nums[i] = y[j]
+                j+=1
+                
+        return nums
+                
