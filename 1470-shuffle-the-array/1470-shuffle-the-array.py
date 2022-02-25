e17504a08fff5f8a6a772d3720d5b74491244aa2
Amlan Alok <amlanalok@gmail.com>
2022-02-25 07:45:03 +0530

    Time: 92 ms (40.23%), Space: 14.2 MB (74.05%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -3,6 +3,11 @@ class Solution:
         
         return self.ans3(nums, n)
     
+    '''
+    This solution which I did using a queue has constant space
+    TC = O(n)
+    SC = 2+2(from queue) = O(1)
+    '''
     def ans2(self, nums: List[int], n: int) -> List[int]:
         from collections import deque
         
@@ -27,7 +32,11 @@ class Solution:
             i+=1
         
         return nums
-        
+    
+    '''
+    TC = O(n)
+    SC = n+n+1 =O(n)
+    '''
     def ans3(self, nums: List[int], n: int) -> List[int]:
         y=[]
         x=len(nums)//2
@@ -36,7 +45,10 @@ class Solution:
             y.append(nums[i+n])
         return(y)
         
-        
+    '''
+    TC = O(n)
+    SC = n + n/2 + n/2 + 2 = O(n)
+    '''
     def ans1(self, nums: List[int], n: int) -> List[int]:
         
         x = nums[:n]
