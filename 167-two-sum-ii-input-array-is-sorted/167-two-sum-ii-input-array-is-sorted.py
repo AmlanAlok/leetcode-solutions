2ab69804aed0ccadad4b94594f9874881fdb7941
Amlan Alok <amlanalok@gmail.com>
2022-03-06 02:10:20 +0530

    Time: 207 ms (18.86%), Space: 15 MB (22.55%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        # return self.ans3(numbers, target)
-        return self.p1(numbers, target)
+        return self.ans3(numbers, target)
+        # return self.p1(numbers, target)
     
     '''
     TC = O(n)
