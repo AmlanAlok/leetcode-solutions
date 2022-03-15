58fd3fb073a9c329f1c8c1c3c9aad48af0cb5912
Amlan Alok <amlanalok@gmail.com>
2022-03-15 16:47:23 +0530

    Time: 1662 ms (7.11%), Space: 17 MB (9.77%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        
+        return self.ans1(s)
+    
+    def ans1(self, s: str) -> str:
+        
+        a=deque()
+        b=deque()
+        ans=[]
+        
+        for i, c in enumerate(s):
+            
+            if a and c==')' and a[-1]=='(':
+                a.pop()
+                b.pop()
+                continue
+            elif c=='(' or c==')':    
+                a.append(c)
+                b.append(i)
+            
+        if len(a)==0:
+            return s
+
+        # for i,v in enumerate(b):
+            # if i>=1:
+            #     # v=v-b[i-1]
+            #     v=v-1
+            # s = s[:v] + s[v+1:]
+            # s[i] = '.'
+        for i, v  in enumerate(s):
+            
+            if i not in b:
+                ans.append(v)
+            
+            
+        print(ans)
+
+        return ''.join(ans)
+                
+            
+            
+        
