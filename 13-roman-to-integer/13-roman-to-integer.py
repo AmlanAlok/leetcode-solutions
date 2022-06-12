4e32d728d558bada306ff7005f4f4745dcdff1c0
Amlan Alok <amlanalok@gmail.com>
2022-06-12 11:54:23 +0530

    Time: 69 ms (48.56%), Space: 13.9 MB (29.44%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -3,8 +3,12 @@
 # "MCMXCIV"
 class Solution:
     def romanToInt(self, s: str) -> int:
-        return self.ans1(s)
+        return self.ans2(s)
     
+    '''
+    TC = O(1) and not O(n) bcuz the max possible number is 3999 as given here.
+    SC = O(1)
+    '''
     def ans1(self, s: str) -> int:
         
         d = {
@@ -39,7 +43,34 @@ class Solution:
         
         return ans
                 
-                
+        
+    '''
+    TC = 1
+    SC = 1
+    This approach is much better and optimized
+    '''
+    def ans2(self, s: str) -> int:
+        d = {
+                'I': 1,
+                'V': 5,
+                'X': 10,
+                'L': 50,
+                'C': 100,
+                'D': 500,
+                'M': 1000
+            }
+        
+        last = len(s)-1
+        total = d[s[last]]
+        
+        for i in range(len(s)-2, -1, -1):
+            if d[s[i]] < d[s[i+1]]:
+                total -= d[s[i]]
+            else:
+                total += d[s[i]]
+        
+        return total
+
                 
                 
             
