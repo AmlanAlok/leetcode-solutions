ca20a78bd2c7dea34459dadf0d53b1b4964cabc5
Amlan Alok <amlanalok@gmail.com>
2022-06-13 02:39:14 +0530

    Time: 87 ms (27.27%), Space: 14 MB (34.44%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,55 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        return self.ans1(num)
+    
+    def ans1(self, num: int) -> str:
+        n = num
+        
+        d = {
+            1: 'I',
+            4: 'IV',
+            5: 'V',
+            9: 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000: 'M'
+        }
+        
+        arr = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+        t = []
+        
+        i = len(arr)-1
+        
+        while i >=0:
+            a = arr[i]
+            
+            if n >= a:
+                n -= a
+                t.append(a)
+            
+            if n < a:
+                i -= 1
+        
+        print(t)
+        
+        ans=[]
+        
+        for v in t:
+            
+            ans.append(d[v])
+            
+            
+        print(ans)
+        
+        return ''.join(ans)
+            
+            
+        
+        
+        
