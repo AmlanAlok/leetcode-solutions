01789d57633a6b72609a458cc3fe203af5457370
Amlan Alok <amlanalok@gmail.com>
2022-06-18 14:40:58 +0530

    Time: 95 ms (19.89%), Space: 14.6 MB (45.83%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -80,6 +80,32 @@ class Solution:
         # print(rev)
         # print(f)
         return rev == f
-           
+    
+    def p1(self, s: str) -> bool:
+        
+        i, j = 0, len(s)-1
+        
+        while i<j:
+            
+            while i<j and not s[i].isalnum():
+                i += 1
+            while i<j and not s[j].isalnum():
+                j -= 1
+            
+            if s[i].lower() != s[j].lower():
+                return False
+            
+            i+=1
+            j-=1
+            
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
            
            
