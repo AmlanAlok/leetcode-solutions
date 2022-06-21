e703e247c6b549a4ddf5d91ce3b6d9253e37ca04
Amlan Alok <amlanalok@gmail.com>
2022-06-21 12:08:19 +0530

    Time: 124 ms (83.99%), Space: 16.1 MB (28.51%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,138 @@
+'''
+[0,1,0,2,1,0,1,3,2,1,2,1]
+[4,2,0,3,2,5]
+[4,2,3]
+'''
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        return self.ans1(height)
+    
+    '''
+    Two pointer
+    TC = n
+    SC = 1
+    '''
+    def ans1(self, h: List[int]) -> int:
+        
+        i, j = 0, len(h)-1
+        left_max, right_max = 0, 0
+        
+        ans = 0
+        
+        while i<j:
+            
+            p, q = h[i], h[j]
+            
+            if p < q:
+                if p >= left_max:
+                    left_max = p
+                else:
+                    ans += left_max - p
+                
+                i+=1
+            else:
+                if q >= right_max:
+                    right_max = q
+                else:
+                    ans += right_max - q
+                
+                j-=1
+                
+        return ans
+    
+    def fail1(self, h: List[int]) -> int:
+        
+        i = 0
+        l = len(h)
+        
+        for k in range(l):
+            if h[k] > 0:
+                i = k
+                break
+        
+        j, t, w = i, 0, 0
+        
+        while i < l and j < l:
+            
+            p, q = h[i], h[j]
+            
+            if p > q:
+                t += p-q
+                if j == l-1 and t != 0:
+                    i += 1
+                    j = i
+                    t = 0
+                # else:
+                    # j+=1
+            elif p <= q:
+                i = j
+                w += t
+                t = 0
+            
+            j+=1
+            
+            # elif j == l-1 and t != 0:
+            #     i += 1
+            #     j = i
+            #     t = 0
+            
+            # if p == q:
+            #     j+=1
+            
+        w += t
+        
+        return w
+    
+    def fail2(self, h: List[int]) -> int:
+        
+        i, j = 0, len(h)-1
+        left_max, right_max = 0, 0
+        
+        lt, rt, w = 0, 0, 0
+        
+        while i<j:
+            
+            p, q = h[i], h[j]
+            
+            if q > right_max:
+                w += rt
+                rt = 0
+                right_max = q
+            
+            if q < right_max:
+                rt += right_max - q
+                
+            if p > left_max:
+                w += lt
+                lt = 0
+                left_max = p
+                
+            if p < left_max:
+                lt += left_max - p
+            
+            if p >= q:
+                j-=1
+            
+            if p < q:
+                i+=1
+        
+        return w
+                    
+                
+        
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                
+        
