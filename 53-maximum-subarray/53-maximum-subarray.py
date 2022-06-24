730f2bfb025acdb08437f30e576957f6aa114f66
Amlan Alok <amlanalok@gmail.com>
2022-06-24 19:49:58 +0530

    Time: 1162 ms (33.19%), Space: 28 MB (38.77%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,6 +1,12 @@
+'''
+[-2,1,-3,4,-1,2,1,-5,4]
+[5,4,-1,7,8]
+[1]
+[1,2]
+'''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        return self.ans1(nums)
+        return self.p1(nums)
     
     '''
     TC = n
@@ -21,7 +27,22 @@ class Solution:
         
         return m
 
+    def p1(self, nums: List[int]) -> int:
+        
+        maxv = -sys.maxsize
+        csum = 0
+        
+        for i in nums:
             
+            if i+csum < i:
+                csum = i
+            else:
+                csum += i
+                
+            if csum > maxv:
+                maxv = csum
+            
+        return maxv
             
         
         
