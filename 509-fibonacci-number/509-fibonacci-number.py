e10e4e4cb29ca90e5bb9bb2d5cef04ff00fb3068
Amlan Alok <amlanalok@gmail.com>
2022-07-06 10:43:17 -0500

    Time: 50 ms (54.11%), Space: 14 MB (9.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        return ans2(n)
-    
+        return ans3(n)
+
+'''
+Bottom up
+TC = n
+SC = n
+'''
 def ans1(n):
     
     a = [None]*(n+1)
@@ -19,6 +24,11 @@ def ans1(n):
         
     return a[n]
 
+'''
+DP
+TC = n
+SC = n
+'''
 def ans2(n, memo={}):
     
     if n in memo:
@@ -27,6 +37,20 @@ def ans2(n, memo={}):
         return 0
     if n == 1:
         return 1
+    
+    memo[n] = ans2(n-1, memo) + ans2(n-2, memo)
+    
+    return memo[n] 
+
+'''
+DP
+TC = n
+SC = n
+'''
+def ans3(n, memo={0:0,1:1}):
+    
+    if n in memo:
+        return memo[n]
     
     memo[n] = ans2(n-1, memo) + ans2(n-2, memo)
     
