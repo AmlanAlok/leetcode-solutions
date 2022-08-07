7e5d59577a1a8be688c44cf3182e01fcff229a64
Amlan Alok <amlanalok@gmail.com>
2022-08-07 15:54:31 -0500

    Time: 123 ms (43.76%), Space: 15.1 MB (50.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,15 @@
+'''
+[2,7,11,15]
+9
+[1,2,3,7,3]
+6
+[3,3]
+6
+'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        return self.p1(nums, target)
+        return self.p3(nums, target)
     
     def ans_1(self, nums: List[int], target: int) -> List[int]:
         
@@ -45,5 +53,19 @@ class Solution:
         
         return []
     
-    
+    def p3(self, nums: List[int], target: int) -> List[int]:
         
+        d = {}
+        t = target
+        
+        for i,v in enumerate(nums):
+            c = t-v
+            if c in d and d[c] != i:
+                return [i,d[c]]
+            else:
+                d[v]=i
+        
+        return [-1,-1]
+            
+            
+    
